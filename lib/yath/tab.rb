2500b74ff1e4fd062d54008e20fd88e5d55ba431
alkcxy <alkcxy@gmail.com>
2013-05-19 19:52:21 +0200

    added some options

--- lib/yath/tab.rb
+++ lib/yath/tab.rb
@@ -7,5 +7,9 @@ module Yath
       @opts = opts
       @opts.reverse_merge! method: "get"
     end
+    
+    def link_to
+      link_to(@title,@path,@opts)
+    end
   end
 end
--- lib/yath/tabs.rb
+++ lib/yath/tabs.rb
@@ -1,12 +1,14 @@
 module Yath
   require 'action_view'
   class Tabs
-    attr_reader :tabs, :block, :current_path
+    attr_reader :tabs, :block, :current_path, :active
     include ActionView::Helpers
 
     def initialize opts, &block
       @tabs = []
       @current_path = opts[:current_path]
+      @active = opts[:active] || "active"
+      @class_name = opts[:class_name] || "nav nav-tabs"
       #@block = instance_eval(&block)
       if block.arity == 1               # the arity() check
         @block = block[self]            # argument expected, pass the object
@@ -20,21 +22,11 @@ module Yath
     end
     
     def do_tabs
-      "<ul>"+@tabs.map do |tab|
+      "<ul class=\"#{@class_name}\">"+@tabs.map do |tab|
         if current_path == tab.path && tab.opts[:method] == "get"
-          %Q(<li class="current"><span>#{tab.title}</span></li>)
+          %Q(<li class="#{@active}"><span>#{tab.title}</span></li>)
         else
-          %Q(<li>#{link_to tab.title, tab.path, tab.opts}</li>)
-        end
-      end.join(' ')+"</ul>"
-    end
-
-    def do_ajax_tabs
-      "<ul>"+@tabs.map do |tab|
-        if current_path == tab.path
-          "<li>"+link_to(tab.title, "#current-tab", tab.opts)+"</li>"
-        else
-          "<li>"+link_to(tab.title, tab.path, tab.opts)+"</li>"
+          %Q(<li>#{tab.link_to}</li>)
         end
       end.join(' ')+"</ul>"
     end
